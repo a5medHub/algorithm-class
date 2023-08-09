1fc1457c0e0cb4789bf2786173fef03ed39b7af8
A.Yassine <ayassine.auce@gmail.com>
2023-08-09 20:22:36 +0300

    add template

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,11 @@ function twoSum(nums, target) {
         for (let j in nums){
             // console.log(nums[j])
             if(nums[i]+nums[j] == target){
-                return [nums[i], nums[j]]
+                return `${nums[i]} + ${nums[j]} = ${target}`
             }
         }
     }
 }
+console.log(twoSum([2,7,11,15],9))
+console.log(twoSum([3,2,4],6))
+console.log(twoSum([3,3],6))
