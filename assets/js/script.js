574513c2e9285b62c1ae5ff85c33b29512709277
A.Yassine <ayassine.auce@gmail.com>
2023-08-09 20:15:09 +0300

    make if condition works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 function twoSum(nums, target) {
     for (let i in nums){
-        console.log(nums[i])
+        // console.log(nums[i])
+        for (let j in nums){
+            // console.log(nums[j])
+            if(nums[i]+nums[j] == target){
+                return [nums[i], nums[j]]
+            }
+        }
     }
 }
